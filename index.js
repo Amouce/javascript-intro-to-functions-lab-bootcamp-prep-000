6aa1001d6ba53e87ea78a22d7c883b1b36f70a3a
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-21 05:29:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ console.log ('hello')
 
 function sayHiToGrandma(string) {
 if ("hello".toLowerCase === lowercase)
-return "I can\'t hear you!" if ("HELLO".toUpperCase === uppercase)
+return "I can\'t hear you!" 
+if ("HELLO".toUpperCase === uppercase)
 return "YES INDEED!"
-
 if ('I love you, Grandma.') 
 return "I love you too"} 
 
