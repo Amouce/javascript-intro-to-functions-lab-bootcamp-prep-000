21c29e7a36b242c7d5ed8e903c3e57c27d3b3181
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-21 05:44:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ function sayHiToGrandma(string)
 {
 if ('hello')
 return "I can\'t hear you!"
+
 if ('HELLO')
 return "YES INDEED!"
 
-
-return "I love you too"
 if ('I love you, Grandma.') 
+return "I love you too"
+
 }
