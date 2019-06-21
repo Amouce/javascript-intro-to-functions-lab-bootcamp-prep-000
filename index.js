52e790a2c9b77e55dcf8285a09cb1e715504350a
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-21 05:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ console.log ('hello')
 }
 
 function sayHiToGrandma(string) {
-if (whisper)
+if ('hello')
 return "I can\'t hear you!" 
-if (shout)
+if ('HELLO')
 return "YES INDEED!"
-if ("I love you, Grandma.") 
+if ('I love you, Grandma.') 
 return "I love you too"} 
 
