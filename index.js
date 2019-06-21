5e2b4e8206aeb0497ee80fce21ac17078af06829
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-21 04:24:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string) {
 return string.toUpperCase()
 console.log(shout)
   }
+  function logshout(string) {
+return string.toUpperCase()
+console.log(shout)
+  }
   function whisper(string) {
 return string.toLowerCase()
 console.log (whisper)
